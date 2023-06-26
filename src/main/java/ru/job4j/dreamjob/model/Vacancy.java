7658c721a873765c9bc533d70a7959de4d17812c
Oklevet <75887570+Oklevet@users.noreply.github.com>
2023-06-26 19:27:47 +0500

    4. Thymeleaf, Циклы. [#504841  [#504841]]

---
+++ src/main/java/ru/job4j/dreamjob/model/Vacancy.java
@@ -0,0 +1,80 @@
+package ru.job4j.dreamjob.model;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Vacancy {
+    private int id;
+    private String title;
+    private String description;
+    private LocalDateTime creationDate;
+
+    public Vacancy(int id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public Vacancy() {
+        super();
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Vacancy vacancy = (Vacancy) o;
+        return id == vacancy.id && Objects.equals(title, vacancy.title) && Objects.equals(description, vacancy.description) && Objects.equals(creationDate, vacancy.creationDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, description, creationDate);
+    }
+}
